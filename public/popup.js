058ded28eb20aba90e76a98e43269fb4800e94df
Wilson Staley <wilstaley@gmail.com>
2019-09-23 18:06:15 -0400

    added notifications

--- public/popup.js
+++ public/popup.js
@@ -1,15 +1,7 @@
-let changeColor = document.getElementById('changeColor');
+let btn = document.getElementById('btn');
 
-  chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-  });
+btn.addEventListener('click', learnMore);
 
-  changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-  };
+function learnMore(){
+    console.log('logged in the popup console...')
+}
--- src/background.js
+++ src/background.js
@@ -1,14 +1,46 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+console.log('background script running 🏃‍');
+
+const sitesWithAI = [
+    'https://www.facebook.com',
+    'https://www.travelocity.com',
+    'https://www.amazon.com'
+];
+
+//query info for getting active tab url
+const queryInfo = {
+    'active': true, 
+    'lastFocusedWindow': true
+};
+
+//options for the notification
+const notificationOptions = {
+    type: "basic",
+    title: "AI Detected!",
+    message: "AI was detected on this site!  Check the AI extension to learn more.",
+    iconUrl: "../public/images/logo.png"
+};
+
+//get the current tab's url
+chrome.tabs.query(queryInfo, function(tabs){
+    let prevPathArray = tabs[0].url.split('/');
+    let prev_url = prevPathArray[0] + '//' + prevPathArray[2];
+    //listen for when a tab is updated (we want to see when a new site is visited)
+    chrome.tabs.onUpdated.addListener(function(){
+        //get the new current url
+        chrome.tabs.query(queryInfo, function(tabs){
+            let pathArray = tabs[0].url.split('/');
+            let new_url = pathArray[0] + '//' + pathArray[2];
+            //if the user has visited a new site...
+            if(!(new_url === prev_url)){
+                console.log('new site!');
+                console.log(new_url);
+                //if the site they just visited is one with AI, notify them
+                if(sitesWithAI.includes(new_url)){
+                    console.log('AI detected...');
+                    chrome.notifications.create(notificationOptions);
+                }
+            }
+            prev_url = new_url;
         })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-});
+    })
+})
