70efaf01b4f5e634d90c208182c98c1765e89f48
Wilson Staley <wilstaley@gmail.com>
2019-10-14 12:58:12 -0400

    added tooltip customizations

--- src/content.js
+++ src/content.js
@@ -2,40 +2,51 @@ console.log('content script running 🏃‍');
 
 const logoURL = chrome.extension.getURL('public/images/logo.png').toString();
 
-//You can use this as the tippy content if you want the logo in the tooltip
-// `<img src=${logoURL} style="margin-bottom: -20px;"> <p>Some text.<p>`,
+const tooltipHTML = `
+    <div id='aiedu-ttwrapper'>
+        <div id='aiedu-ttheader'>
+            <h1>Header</h1>
+        </div>
+        <div id='aiedu-ttmsg'>
+            <p>
+                ___ is using ___ to ____.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque augue felis. Maecenas hendrerit massa risus. Maecenas et auctor quam, non feugiat elit. Sed tristique feugiat tortor ut feugiat. Quisque iaculis tincidunt tortor, non consectetur mauris lobortis a. Fusce risus purus, auctor vel interdum id, dapibus ac est. In sem odio, dictum sed ex id, commodo dictum nibh. Sed consequat viverra ipsum ac euismod. Integer vulputate lorem ac massa ultricies, id finibus diam porttitor. In in turpis sit amet velit efficitur sollicitudin. Duis sit amet posuere dolor.
+            </p>
+        </div>
+    </div>
+`;
 
 chrome.runtime.onMessage.addListener(receiver);
 
 function enableTooltips(){
     //Facebook tooltips
     tippy('#pagelet_marketplace_recently_viewed_rhc', {
-        content: 'Facebook is using your viewing history to suggest items in marketplace.',
+        content: tooltipHTML,
         placement: 'top',
         arrow: true,
-        arrowType: 'round',
         animation: 'shift-away',
-        duration: 500
+        duration: 500,
+        theme: 'aiedu'
     });
     
     tippy('#pagelet_ego_pane', {
-        content: 'Facebook is using your browsing history to create targeted ads.',
+        content: tooltipHTML,
         placement: 'top',
         arrow: true,
-        arrowType: 'round',
         animation: 'shift-away',
         duration: 500,
-        interactive: true
+        interactive: true,
+        theme: 'aiedu'
     });
     
     tippy('._666k', {
-        content: 'Like button!',
+        content: tooltipHTML,
         placement: 'bottom',
         arrow: true,
-        arrowType: 'round',
         animation: 'shift-away',
         duration: 500,
-        interactive: true
+        interactive: true,
+        theme: 'aiedu'
     });
 }
 
