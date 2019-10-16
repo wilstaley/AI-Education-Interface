badde3db65ca74e329d4d7809fe327db90cc3513
Wilson Staley <wilstaley@gmail.com>
2019-10-16 13:57:17 -0400

    function to generate tooltip content

--- src/content.js
+++ src/content.js
@@ -2,51 +2,57 @@ console.log('content script running 🏃‍');
 
 const logoURL = chrome.extension.getURL('public/images/logo.png').toString();
 
-const tooltipHTML = `
-    <div id='aiedu-ttwrapper'>
-        <div id='aiedu-ttheader'>
-            <h1>Header</h1>
+function getTooltipContent(header, site, aitype, reason) {
+    const tooltipHTML = `
+        <link href="https://fonts.googleapis.com/css?family=Barlow|Jura:700&display=swap" rel="stylesheet">
+        <div id='aiedu-ttwrapper'>
+            <div id='aiedu-ttheader' style="font-family:'Jura';">
+                <h1>${header}</h1>
+            </div>
+            <div id='aiedu-ttmsg' style="font-family:'Barlow';">
+                <p>
+                    ${site} is using ${aitype} to ${reason}.
+                </p>
+                <button>learn more!</button>
+            </div>
         </div>
-        <div id='aiedu-ttmsg'>
-            <p>
-                ___ is using ___ to ____.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque augue felis. Maecenas hendrerit massa risus. Maecenas et auctor quam, non feugiat elit. Sed tristique feugiat tortor ut feugiat. Quisque iaculis tincidunt tortor, non consectetur mauris lobortis a. Fusce risus purus, auctor vel interdum id, dapibus ac est. In sem odio, dictum sed ex id, commodo dictum nibh. Sed consequat viverra ipsum ac euismod. Integer vulputate lorem ac massa ultricies, id finibus diam porttitor. In in turpis sit amet velit efficitur sollicitudin. Duis sit amet posuere dolor.
-            </p>
-        </div>
-    </div>
-`;
+    `;
+    return tooltipHTML;
+}
 
 chrome.runtime.onMessage.addListener(receiver);
 
 function enableTooltips(){
     //Facebook tooltips
     tippy('#pagelet_marketplace_recently_viewed_rhc', {
-        content: tooltipHTML,
-        placement: 'top',
+        content: getTooltipContent(
+            'Targeted Ads', 
+            'Facebook', 
+            'deep learning', 
+            'predict how susceptible you will be to an ad’s message using profile demographics, Facebook activity, and activity on websites off of Facebook'
+        ),
+        placement: 'left',
         arrow: true,
         animation: 'shift-away',
         duration: 500,
-        theme: 'aiedu'
+        theme: 'aiedu',
+        interactive: true
     });
     
     tippy('#pagelet_ego_pane', {
-        content: tooltipHTML,
-        placement: 'top',
+        content: getTooltipContent(
+            'Targeted Ads',
+            'Facebook',
+            'deep learning',
+            'predict how susceptible you will be to an ad’s message using profile demographics, Facebook activity, and activity on websites off of Facebook'
+        ),
+        placement: 'left',
         arrow: true,
         animation: 'shift-away',
         duration: 500,
         interactive: true,
-        theme: 'aiedu'
-    });
-    
-    tippy('._666k', {
-        content: tooltipHTML,
-        placement: 'bottom',
-        arrow: true,
-        animation: 'shift-away',
-        duration: 500,
-        interactive: true,
-        theme: 'aiedu'
+        theme: 'aiedu',
+        interactive: true
     });
 }
 
