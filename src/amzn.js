aad2a1a89148426cb634668b9ec458f98b5ddaf2
Wilson Staley <wilstaley@gmail.com>
2019-11-22 17:11:49 -0500

    added more tooltips

--- src/amzn.js
+++ src/amzn.js
@@ -1,9 +1,9 @@
 function enableAmznTooltips(){
     tippy('.nav-search-field', {
         content: getTooltipContent(
-            'Search', 
-            'Amazon', 
-            'machine learning algorithms', 
+            'Search',
+            'Amazon',
+            'machine learning algorithms',
             'recognize patterns from your search history and provide more relevant product suggestions',
             'open-radio-8'
         ),
@@ -14,12 +14,12 @@ function enableAmznTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('.similarities-aui-carousel', {
         content: getTooltipContent(
-            'Recommendations', 
-            'Amazon', 
-            'neural networks', 
+            'Recommendations',
+            'Amazon',
+            'neural networks',
             'generate personalized product recommendations',
             'open-radio-7'
         ),
@@ -30,12 +30,12 @@ function enableAmznTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('.product-shoveler', {
         content: getTooltipContent(
-            'Recommendations', 
-            'Amazon', 
-            'neural networks', 
+            'Recommendations',
+            'Amazon',
+            'neural networks',
             'generate personalized product recommendations',
             'open-radio-7'
         ),
@@ -46,14 +46,62 @@ function enableAmznTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('#desktop-grid-1', {
         content: getTooltipContent(
-            'Recommendations', 
-            'Amazon', 
-            'neural networks', 
+            'Recommendations',
+            'Amazon',
+            'neural networks',
             'generate personalized product recommendations',
             'open-radio-7'
+        ),
+        placement: 'top',
+        arrow: true,
+        animation: 'shift-away',
+        duration: 500,
+        theme: 'aiedu',
+        interactive: true
+    });
+
+    tippy('#sims-fbt-container', {
+        content: getTooltipContent(
+            'Frequently Bought Together',
+            'Amazon',
+            'machine learning',
+            'provide items to you based on data collected from purchases made by customers',
+            'open-radio-9'
+        ),
+        placement: 'top',
+        arrow: true,
+        animation: 'shift-away',
+        duration: 500,
+        theme: 'aiedu',
+        interactive: true
+    });
+
+    tippy('.a-box-inner', {
+        content: getTooltipContent(
+            'Pricing',
+            'Amazon',
+            'machine learning',
+            'manage the dynamic pricing model that lowers prices when demand is low and inventory is high, and increase prices when supply is low',
+            'open-radio-10'
+        ),
+        placement: 'left',
+        arrow: true,
+        animation: 'shift-away',
+        duration: 500,
+        theme: 'aiedu',
+        interactive: true
+    });
+
+    tippy('#customer-reviews_feature_div', {
+        content: getTooltipContent(
+            'Reviews',
+            'Amazon',
+            'machine learning',
+            'identify fake reviews from genuine ones and rank the reviews for you',
+            'open-radio-11'
         ),
         placement: 'top',
         arrow: true,
--- src/fb.js
+++ src/fb.js
@@ -5,21 +5,21 @@
 //           el.getAttribute('data-prev-alt') === el.getAttribute('alt')) {
 //           return;
 //         }
-// 
+//
 //         el.setAttribute('data-prev-alt', el.alt);
-// 
+//
 //         const altText = el.alt;
 //         const isCVTag = altText.startsWith('Image may contain:');
-// 
+//
 //         if (isCVTag) {
 //             console.log(altText);
 //             console.log(el);
 //             el.classList.add('CVTag');
 //             tippy('.CVTag', {
 //                 content: getTooltipContent(
-//                     'Im tired', 
-//                     'Facebook', 
-//                     'machine learning', 
+//                     'Im tired',
+//                     'Facebook',
+//                     'machine learning',
 //                     'predict how susceptible you will be to an ad’s message using profile demographics, Facebook activity, and activity on websites off of Facebook'
 //                 ),
 //                 placement: 'left',
@@ -35,7 +35,7 @@
 
 
 function enableFbTooltips(){
-    
+
     // const observer = new MutationObserver(mutations => {
     //     mutations.forEach(mutation => handleImgs());
     // });
@@ -45,14 +45,14 @@ function enableFbTooltips(){
     //     characterData: false
     // };
     // observer.observe(document.body, config);
-    // 
+    //
     // handleImgs();
-    
+
     tippy('#pagelet_marketplace_recently_viewed_rhc', {
         content: getTooltipContent(
-            'Targeted Ads', 
-            'Facebook', 
-            'machine learning', 
+            'Targeted Ads',
+            'Facebook',
+            'machine learning',
             'predict how susceptible you will be to an ad’s message using profile demographics, Facebook activity, and activity on websites off of Facebook',
             'open-radio-2'
         ),
@@ -63,7 +63,7 @@ function enableFbTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('#pagelet_ego_pane', {
         content: getTooltipContent(
             'Targeted Ads',
@@ -80,7 +80,7 @@ function enableFbTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('.userContent', {
         content: getTooltipContent(
             'Text in Posts',
@@ -97,7 +97,7 @@ function enableFbTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-    
+
     tippy('._666h', {
         content: getTooltipContent(
             'Comments',
@@ -114,4 +114,21 @@ function enableFbTooltips(){
         theme: 'aiedu',
         interactive: true
     });
+
+    // tippy('#topnews_main_stream_408239535924329', {
+    //     content: getTooltipContent(
+    //         'NEWSFEED',
+    //         'Facebook',
+    //         'deep learning',
+    //         'understand how people are using slang in order to match you with advertisers, rid prohibited content, rank search results and identify trending topics',
+    //         'open-radio-5'
+    //     ),
+    //     placement: 'bottom',
+    //     arrow: true,
+    //     animation: 'shift-away',
+    //     duration: 500,
+    //     interactive: true,
+    //     theme: 'aiedu',
+    //     interactive: true
+    // });
 }
