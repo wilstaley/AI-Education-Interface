390f57631bab3ddfa30b366290c140008ae43fb8
Wilson Staley <wilstaley@gmail.com>
2019-11-25 23:40:53 -0500

    cleaned up unnecessary code

--- src/fb.js
+++ src/fb.js
@@ -1,53 +1,4 @@
-// function handleImgs(){
-//     const images = [...document.getElementsByTagName('img')];
-//     images.forEach(el => {
-//         if (el.hasAttribute('data-prev-alt') &&
-//           el.getAttribute('data-prev-alt') === el.getAttribute('alt')) {
-//           return;
-//         }
-//
-//         el.setAttribute('data-prev-alt', el.alt);
-//
-//         const altText = el.alt;
-//         const isCVTag = altText.startsWith('Image may contain:');
-//
-//         if (isCVTag) {
-//             console.log(altText);
-//             console.log(el);
-//             el.classList.add('CVTag');
-//             tippy('.CVTag', {
-//                 content: getTooltipContent(
-//                     'Im tired',
-//                     'Facebook',
-//                     'machine learning',
-//                     'predict how susceptible you will be to an ad’s message using profile demographics, Facebook activity, and activity on websites off of Facebook'
-//                 ),
-//                 placement: 'left',
-//                 arrow: true,
-//                 animation: 'shift-away',
-//                 duration: 500,
-//                 theme: 'aiedu',
-//                 interactive: true
-//             });
-//         }
-//     });
-// }
-
-
 function enableFbTooltips(){
-
-    // const observer = new MutationObserver(mutations => {
-    //     mutations.forEach(mutation => handleImgs());
-    // });
-    // const config = {
-    //     attributes: true,
-    //     childList: true,
-    //     characterData: false
-    // };
-    // observer.observe(document.body, config);
-    //
-    // handleImgs();
-
     tippy('#pagelet_marketplace_recently_viewed_rhc', {
         content: getTooltipContent(
             'Targeted Ads',
@@ -114,21 +65,4 @@ function enableFbTooltips(){
         theme: 'aiedu',
         interactive: true
     });
-
-    // tippy('#topnews_main_stream_408239535924329', {
-    //     content: getTooltipContent(
-    //         'NEWSFEED',
-    //         'Facebook',
-    //         'deep learning',
-    //         'understand how people are using slang in order to match you with advertisers, rid prohibited content, rank search results and identify trending topics',
-    //         'open-radio-5'
-    //     ),
-    //     placement: 'bottom',
-    //     arrow: true,
-    //     animation: 'shift-away',
-    //     duration: 500,
-    //     interactive: true,
-    //     theme: 'aiedu',
-    //     interactive: true
-    // });
 }
